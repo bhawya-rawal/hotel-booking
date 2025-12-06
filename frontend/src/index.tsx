c3a01abbafa2cebd088e3bcc6d3ab949d617860c
bhawya-rawal <bhawyarawal99@gmail.com>
2025-12-06 18:30:00 +0530

    Added axios for deployment

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,11 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import axios from 'axios';
+
+if (process.env.REACT_APP_API_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+}
 
 ReactDOM.render(
   <React.StrictMode>
